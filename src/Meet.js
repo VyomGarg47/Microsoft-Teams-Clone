82369bfa1b7be6b3cbdf0a4685c5694772077186
VyomGarg47 <vyomgarg.official@gmail.com>
2021-06-20 21:50:49 +0530

    Added Heroku Link

--- src/Meet.js
+++ src/Meet.js
@@ -41,7 +41,7 @@ class Meet extends Component {
       users: [],
     };
     //DONT FORGET TO CHANGE TO YOUR URL
-    this.serviceIP = "/webrtcPeer";
+    this.serviceIP = "https://webrtc-video-call-test.herokuapp.com/";
 
     this.socket = null;
   }
