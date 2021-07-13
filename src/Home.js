004fac2eee322c220dd0af221428c656e68f748a
VyomGarg47 <vyomgarg.official@gmail.com>
2021-07-13 14:24:24 +0530

    Updated UI

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
 import "./Home.css";
 import Picture1 from "./images/Picture7.png";
+import AddIcon from "@material-ui/icons/Add";
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -23,9 +24,14 @@ class Home extends Component {
       url = this.state.url.split("/");
       window.location.href = `/${url[url.length - 1]}`;
     } else {
-      url = Math.random().toString(36).substring(2, 7);
-      window.location.href = `/${url}`;
+      alert("URL cannot be empty");
     }
+  };
+
+  create = () => {
+    var url;
+    url = Math.random().toString(36).substring(2, 10);
+    window.location.href = `/${url}`;
   };
 
   render() {
@@ -65,24 +71,51 @@ class Home extends Component {
               background: "white",
               width: "27%",
               height: "auto",
-              padding: "10px",
+              padding: "15px",
               minWidth: "325px",
               textAlign: "center",
               marginTop: "50px",
             }}
           >
-            <p style={{ margin: 0, fontWeight: "bold", fontSize: "20px" }}>
-              Start or join a meeting
+            <Button variant="contained" color="primary" onClick={this.create}>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                }}
+              >
+                <AddIcon />
+                <span>
+                  <p style={{ fontSize: "15px", margin: 0 }}>
+                    Create a new Room
+                  </p>
+                </span>
+              </div>
+            </Button>
+            <p
+              style={{
+                margin: 0,
+                fontWeight: "bold",
+                fontSize: "20px",
+                marginTop: 15,
+              }}
+            >
+              or Join a room
             </p>
-            <div>
+            <div style={{ marginTop: 15 }}>
               <Input placeholder="URL" onChange={(e) => this.handleChange(e)} />
               <Button
-                variant="contained"
                 color="primary"
                 onClick={this.join}
-                style={{ margin: "20px", width: "70px" }}
+                style={{
+                  marginLeft: "20px",
+                  width: "70px",
+                  backgroundColor: "#ebf4ff",
+                  //border: "2px solid #3f51b5",
+                }}
               >
-                Go
+                <p style={{ margin: 0, fontWeight: "bold" }}>Join</p>
               </Button>
             </div>
           </div>
